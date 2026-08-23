ae6f46e9b8de1e7aadbe971f37d919d3e5ac8a12
Aman Sunesh <as18181@nyu.edu>
2026-08-24 01:55:41 +0400

    Refactor virtual environment creation check

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = "Stop"
 
 $Root = Split-Path -Parent $PSScriptRoot
 Set-Location $Root
-if (-not (Test-Path ".venv")) { python -m venv .venv }
 
 if (-not (Test-Path ".venv")) {
     python -m venv .venv
